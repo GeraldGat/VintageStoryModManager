aa7e8f3e61d56c3b081d58c4b1f76f2736e910c7
GeraldGat <gerald.gatouillat@gmail.com>
2025-05-04 12:40:07 +0200

    Dependency Injection Setup

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,37 @@ namespace VintageStoryModManager
     /// </summary>
     public partial class App : Application
     {
-    }
+        public static IHost? AppHost { get; private set; }
 
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    // Views
+                    services.AddTransient<MainWindow>();
+
+                    // Services
+
+                    // ViewModels
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
+    }
 }
